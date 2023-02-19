4c8f31c14f19ccef912810aa74cd00e1cdf28f15
Defstrong <sfayzulloev1@gmail.com>
2023-02-19 15:15:08 +0500

    AddDopMaterial

---
+++ Completed_Problems/MonipulationWhisArrays/CheckDoubleExist.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Completed_Problems.MonipulationWhisArrays
+{
+    static public class CheckDoubleExist
+    {
+        static public bool CheckIfExist(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[i] == arr[j] * 2)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
